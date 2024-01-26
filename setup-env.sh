53ecb181d8d37147b0a52590ad43b611991f7b46
Jihee Ryu <chrisjihee@etri.re.kr>
2024-01-26 17:51:08 +0900

    Initial commit

---
+++ setup-env.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# DeepKNLP
+mamba create -n DeepKNLP-24.01 python=3.10 -y
+mamba activate DeepKNLP-24.01
+pip install -r requirements.txt
+pip list
+
+# chrislab (for public user)
+rm -rf chrisbase* chrislab*
+pip download --no-binary :all: --no-deps chrisbase==0.4.7; tar zxf chrisbase-*.tar.gz; rm chrisbase-*.tar.gz;
+pip download --no-binary :all: --no-deps chrislab==0.6.2; tar zxf chrislab-*.tar.gz; rm chrislab-*.tar.gz;
+pip install --editable chrisbase*
+pip install --editable chrislab*
+
+# chrislab (for previleged user)
+rm -rf chrisbase* chrislab*
+git clone https://github.com/chrisjihee/chrisbase.git
+git clone https://github.com/chrisjihee/chrislab.git
+pip install --editable chrisbase*
+pip install --editable chrislab*
+
+# pretrained LM (for public user)
+rm -rf pretrained*
+git lfs install
+git clone https://github.com/KPFBERT/kpfbert pretrained/KPF-BERT
+git clone https://huggingface.co/klue/roberta-base pretrained/KLUE-RoBERTa
+git clone https://huggingface.co/KETI-AIR/ke-t5-base-ko pretrained/KETI-KeT5
+git clone https://huggingface.co/etri-lirs/kebyt5-base-preview pretrained/ETRI-KeByT5
+git lfs uninstall
+
+# pretrained LM (for internal user)
+rm -rf pretrained*
+ln -s ../pretrained-com .  # git clone guest@129.254.164.137:git/pretrained-com
+ln -s ../pretrained-pro .
